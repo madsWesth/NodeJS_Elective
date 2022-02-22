dbd3b7605439732e9a9330f708104f7a45d3f373
madsWesth <6S29FrJJ5up4>
2022-02-22 13:42:21 +0100

    updated REST API with patch and id generation

--- 2._Beers_REST/app.js
+++ 2._Beers_REST/app.js
@@ -1,20 +1,46 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 const beers = [
     {id: 1, types: "Pilsner"},
     {id: 2, brand: "Carlsberg", alcoholPercentage: 4.6},
     {id: 3, expirationDate: new Date()},
 ]
 
+let CURRENT_ID = 3
+
 app.get("/beers", (req, res) => {
     res.send({data: beers})
 })
 
-app.get("beers/:id", (req, res) => {
+app.get("/beers/:id", (req, res) => {
     const foundBeer = beers.find(beer => beer.id === Number(req.params.id))
 
     foundbeer ? res.send({data: foundBeer}) : res.status(404).send()
+})
+
+app.post("/beers", (req, res) => {
+    const beerToAdd = req.body
+    beerToAdd.id = ++CURRENT_ID
+    beers.push({data: beerToAdd})
+})
+
+app.patch("/beers/:id", (req, res) => {
+    const index = beers.findIndex(beer => beer.id === Number(req.params.id))
+    if(index !== -1){
+        const foundBeer = beers[index]
+        const beerToUpdateWith = req.body
+        //spread operator to get all the properties of the object
+        //the last key value pair will overwrite previous key value pairs
+        const updatedBeer = { ...foundBeer, ... newBeer, id : foundBeer.id}
+    
+        beers[index] = updatedBeer
+    } else {
+        res.status(404).send()
+    }
+
 })
 
 app.delete("/beers/:id", (req, res) => {
